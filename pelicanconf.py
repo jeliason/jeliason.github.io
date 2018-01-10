89a5b548125c86a2c14de407b3a43479aa7da275
Joel Eliason <joel.eliason119@gmail.com>
2018-01-10 10:40:43 -0600

    update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,16 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Joel Eliason'
-SITENAME = 'Attracting.Point'
+THEME = '/home/schwarzian/Desktop/pelican-themes/storm'
+AUTHOR = u'Joel Eliason'
+SITENAME = u'Attracting.Point'
 SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'America/Chicago'
 
-DEFAULT_LANG = 'English'
+DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -20,22 +21,16 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#         ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
-DISPLAY_CATEGORIES_ON_MENU = False
-DISPLAY_PAGES_ON_MENU = False
-MENUITEMS = (
-	('About','/about'),
-	('Blog','/'),
-)
